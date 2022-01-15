7b09a7150d854056a59c6fbe8eac11a73a2e8114
Bilal Akbar <muhammadbilalakbar021@gmail.com>
2022-01-15 18:08:42 +0500

    heroku service

--- src/app/services/host.service.ts
+++ src/app/services/host.service.ts
@@ -4,12 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class HostService {
+  ip: string = "https://oaimsadminpanel.herokuapp.com";
   // ip: string = "http://192.168.18.25:3000";
-  ip: string = "http://localhost:3000";
+  // ip: string = "http://localhost:3000";
 
   constructor() { }
 
-  getHostIp(){
+  getHostIp() {
     return this.ip;
   }
 }
