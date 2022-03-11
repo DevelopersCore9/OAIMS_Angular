34b6707ce87919d2eca16fa43a68e037a66c3553
Bilal Akbar <bilal.akbar@kryptomind.com>
2022-03-11 17:11:57 +0500

    Update host.service.ts

--- src/app/services/host.service.ts
+++ src/app/services/host.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HostService {
-  ip: string = "https://oaims-backend.herokuapp.com";
+  ip: string = 'https://api.oaims-tex.com';
+  // ip: string = "https://oaims-backend.herokuapp.com";
   // ip: string = "http://192.168.18.25:3000";
   // ip: string = "http://localhost:3000";
 
-  constructor() { }
+  constructor() {}
 
   getHostIp() {
     return this.ip;
