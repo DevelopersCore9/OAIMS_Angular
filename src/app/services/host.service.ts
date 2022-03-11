81b1f6f408e270473aec2217736823317060038a
Abdur Rehman <abdur@whydonate.nl>
2022-03-12 00:13:16 +0500

    updated

--- src/app/services/host.service.ts
+++ src/app/services/host.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HostService {
-  ip: string = "https://oaims-backend.herokuapp.com";
+  // ip: string = "https://oaims-backend.herokuapp.com";
   // ip: string = "http://192.168.18.25:3000";
-  // ip: string = "http://localhost:3000";
+  ip: string = 'http://localhost:8080';
 
-  constructor() { }
+  constructor() {}
 
   getHostIp() {
     return this.ip;
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -5,18 +5,17 @@ import { Injectable } from '@angular/core';
 import { Observable, defer } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
   constructor(
     public hostAddress: HostService,
     public http: HttpClient,
     protected spin: SpinnerService
-  ) { }
+  ) {}
 
   getTopSelling() {
-    this.spin.changeSpinnerState(true)
+    this.spin.changeSpinnerState(true);
     return new Promise<any>((resolve, reject) => {
       this.http
         .get(`${this.hostAddress.getHostIp()}/api/products/topSelling`)
@@ -26,7 +25,7 @@ export class ProductsService {
           if (data == null) {
             console.log(data);
           }
-          this.spin.changeSpinnerState(false)
+          this.spin.changeSpinnerState(false);
           resolve(data.payload);
         })
         .catch((err) => {
@@ -37,7 +36,7 @@ export class ProductsService {
   }
 
   getAllProducts() {
-    this.spin.changeSpinnerState(true)
+    this.spin.changeSpinnerState(true);
     return new Promise<any>((resolve, reject) => {
       this.http
         .get(`${this.hostAddress.getHostIp()}/api/products?page=1&limit=10`)
@@ -47,7 +46,7 @@ export class ProductsService {
           if (data == null) {
             console.log(data);
           }
-          this.spin.changeSpinnerState(false)
+          this.spin.changeSpinnerState(false);
           resolve(data.payload);
         })
         .catch((err) => {
@@ -58,48 +57,69 @@ export class ProductsService {
   }
 
   CustomProductsCategory(categoryName: string): Observable<any> {
-    let obj = { "category": categoryName }
+    let obj = { category: categoryName };
     // return this.http.post(`${this.hostAddress.getHostIp()}/api/products/category`, obj)
     return defer(() => {
       return new Promise((resolve, reject) => {
-        this.http.post(`${this.hostAddress.getHostIp()}/api/products/category`, obj)
+        this.http
+          .post(`${this.hostAddress.getHostIp()}/api/products/category`, obj)
           .toPromise()
           .then((data: any) => {
-            console.log(data)
+            console.log(data);
             if (data == null) {
-              console.log(data.payload)
+              console.log(data.payload);
             }
-            resolve(data.payload)
+            resolve(data.payload);
           })
           .catch((err) => {
-            console.log(err)
-            reject(err)
-          })
-      })
-    })
-
+            console.log(err);
+            reject(err);
+          });
+      });
+    });
   }
 
   CustomProductsSubCategory(categoryName: string): Observable<any> {
-    let obj = { "subCategory_id": categoryName }
+    let obj = { subCategory_id: categoryName };
     // return this.http.post(`${this.hostAddress.getHostIp()}/api/products/category`, obj)
     return defer(() => {
       return new Promise((resolve, reject) => {
-        this.http.post(`${this.hostAddress.getHostIp()}/api/products/subcategory`, obj)
+        this.http
+          .post(`${this.hostAddress.getHostIp()}/api/products/subcategory`, obj)
           .toPromise()
           .then((data: any) => {
-            console.log(data)
+            console.log(data);
             if (data == null) {
-              console.log(data.payload)
+              console.log(data.payload);
             }
-            resolve(data.payload)
+            resolve(data.payload);
           })
           .catch((err) => {
-            console.log(err)
-            reject(err)
-          })
-      })
-    })
+            console.log(err);
+            reject(err);
+          });
+      });
+    });
+  }
 
+  getProduct(id: string) {
+    this.spin.changeSpinnerState(true);
+    return new Promise<any>((resolve, reject) => {
+      this.http
+        .get(`${this.hostAddress.getHostIp()}/api/products/byId?id=${id}`)
+        .toPromise()
+        .then((data: any) => {
+          console.log(data);
+          if (data == null) {
+            console.log(data);
+          }
+          this.spin.changeSpinnerState(false);
+          resolve(data.payload);
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(err);
+        });
+    });
   }
 }
